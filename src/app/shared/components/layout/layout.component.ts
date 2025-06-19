c27c50e4ff81a4f5d39853979ce9c59eeba9f35e
Vaish-123 <vaishnavnampoothiri@gamil.com>
2025-06-19 20:38:18 +0530

    #6 Layout Component

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { SharedModule } from '../../modules/shared.module';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [SharedModule, RouterModule, SidebarComponent],
+  imports: [SharedModule, RouterModule, SidebarComponent, NgxSpinnerModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  sidebarCollapsed: boolean = false;
 
+  toggleSidebar() {
+    this.sidebarCollapsed = !this.sidebarCollapsed;
+  }
 }
